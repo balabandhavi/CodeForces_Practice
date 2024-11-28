9177298fdc7160f856b7a6602282d0df0f217223
balabandhavi <115206578+balabandhavi@users.noreply.github.com>
2024-11-28 11:09:59 +0530

    Completed

---
+++ A_Watermelon.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class A_Watermelon{
+    public A_Watermelon(int w){
+    }
+
+    public boolean canDivide(int weight){
+        if(weight%2==0) return true;
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int w=sc.nextInt();
+        A_Watermelon a=new A_Watermelon(w);
+        if(a.canDivide(w)){
+            System.out.println("YES");
+        }else 
+        System.out.println("NO");
+        sc.close();
+    }
+}
